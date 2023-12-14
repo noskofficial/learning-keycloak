643a9ed11215e6f8b96a8f6ef32ff483bda3dd71
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2023-12-14 23:16:11 +0545

    feat: send response to the frontend in protected route

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-const Protected = () => {
-  return <div>Protected</div>;
+const Protected = ({ token }) => {
+  const isRun = useRef(false);
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (isRun.current) return;
+
+    isRun.current = true;
+
+    const config = {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios
+      .get("/socials", config)
+      .then((res) => setData(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return data ? (
+    <>
+      <h1>User socials</h1>
+      {Object.entries(data).map(([k, v]) => {
+        return (
+          <div key={k}>
+            {k}: {<a href={v}>{k}</a>}
+          </div>
+        );
+      })}
+    </>
+  ) : (
+    <div>Protected</div>
+  );
 };
 
 export default Protected;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/socials": "http://backend:5000/",
+    },
+  },
   plugins: [react()],
 });
