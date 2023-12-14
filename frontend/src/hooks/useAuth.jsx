ede9c2059151c7183905c3225756931ff8367a24
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2023-12-14 23:06:42 +0545

    feat: add basic login using keycloak

---
+++ frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from "react";
+import Keycloak from "keycloak-js";
+
+const client = new Keycloak({
+  url: import.meta.env.VITE_KEYCLOAK_URL,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT,
+});
+
+const useAuth = () => {
+  const isRun = useRef(false);
+  const [token, setToken] = useState(null);
+  const [isLogin, setLogin] = useState(false);
+
+  useEffect(() => {
+    if (isRun.current) return;
+
+    isRun.current = true;
+    client
+      .init({
+        onLoad: "login-required",
+      })
+      .then((res) => {
+        console.log({ res, client });
+        setLogin(res);
+        setToken(client.token);
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  }, []);
+
+  return [isLogin, token];
+};
+
+export default useAuth;
